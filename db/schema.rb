412dd0500d7c015209c597f1aa516c52786ef32a
Hakeem Baba <keemtaker@yahoo.com>
2017-12-27 01:44:01 +0100

    made controllers, routes and index and new views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171226223905) do
+ActiveRecord::Schema.define(version: 20171226231351) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
