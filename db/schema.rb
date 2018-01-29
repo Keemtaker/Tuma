4d8abd925be26d76453e427d5b9911fa8b31a176
Hakeem Baba <keemtaker@yahoo.com>
2018-01-29 19:12:33 +0100

    perks pretty much done! bravo!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126185219) do
+ActiveRecord::Schema.define(version: 20180127193644) do
 
   create_table "applicants", force: :cascade do |t|
     t.string "first_name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180126185219) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "photo"
+    t.string "logo"
     t.index ["user_id"], name: "index_companies_on_user_id"
   end
 
