332c5130a4c040d8bcf60fd8786e787179ea5dd9
Hakeem Baba <keemtaker@yahoo.com>
2018-03-08 14:14:48 +0100

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,37 +21,37 @@
 
 # p "Creating Perks and Industries"
 
-# industries = ['IT & Software', 'SaaS', 'Finance', 'Healthcare', "Entertainment/Media", "Agency & Consulting", "Hardware", "Fashion", "Energy & Greentech", "Education", "Consumer Goods", "Other"]
-# industries.each do |industry|
-#   Industry.create!(name: industry)
-# end
-# software_industry = Industry.where(name: ["IT & Software", "SaaS", "Hardware"])
-# fashion_industry = Industry.where(name: ["Fashion", "Consumer Goods"])
-# finance_industry = Industry.where(name: ["IT & Software", "SaaS", "Finance", "Agency & Consulting"])
-# hardware_industry = Industry.where(name: ["IT & Software", "Entertainment/Media", "Hardware"])
-# media_industry = Industry.where(name: ["IT & Software", "Entertainment/Media"])
+industries = ['IT & Software', 'SaaS', 'Finance', 'Healthcare', "Entertainment/Media", "Agency & Consulting", "Hardware", "Fashion", "Energy & Greentech", "Education", "Consumer Goods", "Other"]
+industries.each do |industry|
+  Industry.create!(name: industry)
+end
+software_industry = Industry.where(name: ["IT & Software", "SaaS", "Hardware"])
+fashion_industry = Industry.where(name: ["Fashion", "Consumer Goods"])
+finance_industry = Industry.where(name: ["IT & Software", "SaaS", "Finance", "Agency & Consulting"])
+hardware_industry = Industry.where(name: ["IT & Software", "Entertainment/Media", "Hardware"])
+media_industry = Industry.where(name: ["IT & Software", "Entertainment/Media"])
 
-# perks = [ 'Friday Bars', 'Free Lunch', 'Skill Development', 'Free Coffee', 'Flexible Work Hours', 'Career Progression', 'Health Insurance', 'Games & Hangouts', 'Occasional Remote Work', 'Paid Vacation', 'Equity & Stock Options', 'Company Retreats', 'Maternity & Paternity Leave', 'Pet Friendly Office', 'Pension Plan']
-# perks.each do |perk|
-#   Perk.create!(name: perk)
-# end
-# saved_perks = nil
-# saved_perks = Perk.all
+perks = [ 'Friday Bars', 'Free Lunch', 'Skill Development', 'Free Coffee', 'Flexible Work Hours', 'Career Progression', 'Health Insurance', 'Games & Hangouts', 'Occasional Remote Work', 'Paid Vacation', 'Equity & Stock Options', 'Company Retreats', 'Maternity & Paternity Leave', 'Pet Friendly Office', 'Pension Plan']
+perks.each do |perk|
+  Perk.create!(name: perk)
+end
+saved_perks = nil
+saved_perks = Perk.all
 
 
-# apple_description = "Apple products have always been designed for the way we work as much as for the way we live. Today they help employees to work more simply and productively, solve problems creatively, and collaborate with a shared purpose. And they’re all designed to work together beautifully. When people have access to iPhone, iPad, and Mac, they can do their best work and reimagine the future of their business."
-# microsoft_description = "We believe in what people make possible. Our mission is to empower every person and every organization on the planet to achieve more."
-# google_description = "Organize the world’s information and make it universally accessible and useful.
-# Since the beginning, our goal has been to develop services that significantly improve the lives of as many people as possible.
-# Not just for some. For everyone."
+apple_description = "Apple products have always been designed for the way we work as much as for the way we live. Today they help employees to work more simply and productively, solve problems creatively, and collaborate with a shared purpose. And they’re all designed to work together beautifully. When people have access to iPhone, iPad, and Mac, they can do their best work and reimagine the future of their business."
+microsoft_description = "We believe in what people make possible. Our mission is to empower every person and every organization on the planet to achieve more."
+google_description = "Organize the world’s information and make it universally accessible and useful.
+Since the beginning, our goal has been to develop services that significantly improve the lives of as many people as possible.
+Not just for some. For everyone."
 
-# p "Creating Software Companies"
-# software_list = [
-#   {name: "Apple", description: apple_description, website: "https://www.apple.com/", location: "California", address: "1 Infinite Loop; Cupertino, CA 95014", user: User.first, perks: saved_perks.uniq.sample(7), industries: software_industry },
-#   {name: "Microsoft", description: microsoft_description, website: "https://www.microsoft.com/", location: "Washington", address: "Address: 1 Microsoft Way, Redmond, WA 98052, USA", user: User.first, perks: saved_perks.uniq.sample(8), industries: software_industry },
-#   {name: "Google", description: google_description, website: "https://www.google.com/", location: "California", address: "1600 Amphitheatre Parkway", user: User.first, perks: saved_perks.uniq.sample(5), industries: software_industry }]
+p "Creating Software Companies"
+software_list = [
+  {name: "Apple", description: apple_description, website: "https://www.apple.com/", location: "California", address: "1 Infinite Loop; Cupertino, CA 95014", user: User.first, perks: saved_perks.uniq.sample(7), industries: software_industry },
+  {name: "Microsoft", description: microsoft_description, website: "https://www.microsoft.com/", location: "Washington", address: "Address: 1 Microsoft Way, Redmond, WA 98052, USA", user: User.first, perks: saved_perks.uniq.sample(8), industries: software_industry },
+  {name: "Google", description: google_description, website: "https://www.google.com/", location: "California", address: "1600 Amphitheatre Parkway", user: User.first, perks: saved_perks.uniq.sample(5), industries: software_industry }]
 
-# Company.create!(software_list)
+Company.create!(software_list)
 
 # adidas_description = "Employing more than 60,000 people in over 160 countries, we produce more than 850 million product units every year and generate sales of € 19 billion (all figures relate to 2016). These numbers alone can easily suggest that adidas is quite a complex organisation. True. But we keep things simple, lean and fast."
 # nike_description = "Because we exist to serve athletes, we dare to design the future of sport. To us, innovation is about elevating human potential. We obsess the needs of the world’s best athletes, using their insights to create products that are beautiful and useful for everyone. To make big leaps, we take big risks. Incremental change won’t get us to where we want to go fast enough. So we pursue moonshots — like developing the first self-lacing shoe, running the first two-hour marathon, and committing to 100% renewable energy use in our facilities."
@@ -108,12 +108,12 @@
 # # name of  finance companies [Barclays, Stripe, Mastercard]
 # # name of hardware companies [ElectronicArts, Beats, Hewlett-Packard]
 # # name of Media companies [CNN, Watsapp, Twitter  ]
-# Company.all.each do |company|
-#   company.photo = Rails.root.join("app/assets/images/cover/#{company.name.gsub(" ","")}.jpg").open
-#   company.logo = Rails.root.join("app/assets/images/logo/#{company.name.gsub(" ","")}.svg").open
-#   company.save
-# end
-# p "Success!!!"
+ Company.all.each do |company|
+   company.photo = Rails.root.join("app/assets/images/cover/#{company.name.gsub(" ","")}.jpg").open
+   company.logo = Rails.root.join("app/assets/images/logo/#{company.name.gsub(" ","")}.svg").open
+   company.save
+ end
+ p "Success!!!"
 
 # developer_one = Job.find(29).description
 # consultant_one = Job.find(65).description
