80721c69736b6657a4f8eb2618e41db1c5cd05db
Hakeem Baba <keemtaker@yahoo.com>
2018-05-06 10:05:35 +0200

    removed cover letter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180503181723) do
+ActiveRecord::Schema.define(version: 20180506080334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20180503181723) do
     t.string "email"
     t.string "mobile_number"
     t.string "resume"
-    t.string "cover_letter"
     t.index ["job_id"], name: "index_applicants_on_job_id"
   end
 
