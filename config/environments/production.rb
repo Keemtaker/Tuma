6924d9e50fd712fe4aee6c35bfb62cc56a892294
Hakeem Baba <keemtaker@yahoo.com>
2018-06-10 19:59:10 +0200

    default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   :authentication => :plain,
 }
 
-  # config.action_mailer.default_url_options = { host: "touma.herokuapp.com" }
+config.action_mailer.default_url_options = { host: "https://www.tumajobs.com.gh"}
 
 
 
