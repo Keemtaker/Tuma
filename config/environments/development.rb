b906ccac4f0d871e257cdb09d8289729cbb4e292
Hakeem Baba <keemtaker@yahoo.com>
2018-06-11 20:49:14 +0200

    sandbox test for mailgun worked

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,11 @@
 Rails.application.configure do
     # Verifies that versions and hashed value of the package contents in the project's package.json
 
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_key: ENV['API_KEY'],
+    domain: ENV['DOMAIN'],
+  }
   config.action_mailer.default_url_options = { host: 'localhost', port: 9292 }
 
 
